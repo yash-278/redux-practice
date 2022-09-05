ca955cbed3b525aca608328ff61384497cd8d687
yash-278 <yashkadam278@gmail.com>
2022-09-06 00:08:12 +0530

    Implement Login & Redirection

--- src/components/signup/signup.component.jsx
+++ src/components/signup/signup.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { LockClosedIcon, UserCircleIcon } from "@heroicons/react/20/solid";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const handleLogin = (event) => {
+  let navigate = useNavigate();
+
+  const handleSignup = (event) => {
     event.preventDefault();
     const data = {
       username: event.target[0].value,
@@ -10,17 +13,29 @@ const Signup = () => {
     };
     localStorage.setItem("userData", JSON.stringify(data));
     localStorage.setItem("isAuthenticated", true);
-  };
-  return (
-    <>
-      {/*
-      This example requires updating your template:
 
-      ```
-      <html class="h-full bg-gray-50">
-      <body class="h-full">
-      ```
-    */}
+    navigate("home");
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const data = {
+      username: event.target[0].value,
+      password: event.target[1].value,
+    };
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    if (userData.username === data.username && userData.password === data.password) {
+      localStorage.setItem("isAuthenticated", true);
+      navigate("home");
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
+
+  return localStorage.getItem("isAuthenticated") === "true" ? (
+    <Navigate to="home" />
+  ) : (
+    <div>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
@@ -43,7 +58,6 @@ const Signup = () => {
                   Email address
                 </label>
                 <input
-                  id="email-address"
                   name="email"
                   type="email"
                   autoComplete="email"
@@ -57,7 +71,6 @@ const Signup = () => {
                   Password
                 </label>
                 <input
-                  id="password"
                   name="password"
                   type="password"
                   autoComplete="current-password"
@@ -85,7 +98,67 @@ const Signup = () => {
           </form>
         </div>
       </div>
-    </>
+
+      <div className="flex min-h-full items-center justify-center px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h2 className="text-center text-3xl font-bold tracking-tight text-gray-100">
+              Sign up for new account
+            </h2>
+          </div>
+          <form
+            className="mt-8 space-y-6"
+            onSubmit={(event) => {
+              handleSignup(event);
+            }}
+          >
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
